5b4842091b12d409abb00c95997afc923d00f1e5
duckbill413 <uhyeon7399@daum.net>
2024-07-18 02:42:33 +0900

    Session 8 Configuration Service

--- config-server/src/main/java/wh/duckbill/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/wh/duckbill/configserver/ConfigServerApplication.java
@@ -3,10 +3,19 @@ package wh.duckbill.configserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.core.env.Environment;
 
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
+
+//    public ConfigServerApplication(Environment environment) {
+//        System.out.println(environment.getProperty("======================="));
+//        System.out.println(environment.getProperty("======================="));
+//        System.out.println(environment.getProperty("user.home"));
+//        System.out.println(environment.getProperty("======================="));
+//        System.out.println(environment.getProperty("======================="));
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
